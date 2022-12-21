04cb919b9f8dac0ad9d40f75eb934a0ae7ec912d
Riany Mello <mello.riany@gmail.com>
2022-12-21 22:24:52 +0000

    Estado (useState)

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,13 +1,19 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-
+import { useState } from 'react';
 import styles from './Post.module.css'
 
 import { Comment } from '../components/Comment'
 import { Avatar } from './Avatar'
 
 export function Post({ author, publishedAt, content }) {
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
+  
+  
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
       locale: ptBR,
     });
   
@@ -15,6 +21,13 @@ export function Post({ author, publishedAt, content }) {
       locale: ptBR,
       addSuffix: true
     });
+
+    function handleCrateNewComment() {
+      event.preventDefault()
+  
+      setComments([...comments, comments.length + 1]);
+    }
+  
 
     return (
       <article className={styles.post}>
@@ -42,7 +55,7 @@ export function Post({ author, publishedAt, content }) {
         })}
         </div>
   
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
           <strong>Deixe seu feedback</strong>
   
           <textarea
@@ -55,9 +68,9 @@ export function Post({ author, publishedAt, content }) {
         </form>
   
         <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
+        {comments.map(comment => {
+          return <Comment />
+        })}
         </div>
       </article>
     )
